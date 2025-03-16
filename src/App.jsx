a4ca7eb8988728f0cda3e43d0a275049c8e843d3
Dorukhan Bekdur <dorukhanbekdur@gmail.com>
2025-03-16 20:26:12 +0300

    Initialize project structure and add Header component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      <h1 className="text-center mt-10 text-2xl font-bold">Welcome to Xpensa!</h1>
+    </div>
+  )
+}
+
+export default App
